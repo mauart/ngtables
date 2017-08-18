436dadfb3df3307b75a360530d22478331f14662
Mauricio David Arteaga Chavez <mauricio.146@hotmail.es>
2017-08-17 23:58:28 -0500

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2SmartTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
